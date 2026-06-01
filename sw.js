509019695cdf70eb3b4a33dcb12fcacb842e6014
mehmetsahtokay <mehmetsahtokay@outlook.com>
2026-06-02 00:40:04 +0300

    Update service worker cache and file paths

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-const CACHE = 'todo-v1';
-const FILES = ['/', '/index.html'];
-
+const CACHE = 'kafe-v1';
+const FILES = ['/kafe/index.html'];
+ 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
-
+ 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
+ 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/kafe/index.html')))
   );
 });
+ 
